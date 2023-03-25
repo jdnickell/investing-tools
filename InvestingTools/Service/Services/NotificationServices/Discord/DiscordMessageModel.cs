bfeb6ff11615339b6b658da7c6f5367da46a2340
JD Nickell <jd@jdnickell.com>
2023-03-26 06:15:31 +0900

    refactor: delete old solution directory

--- InvestingTools/Service/Services/NotificationServices/Discord/DiscordMessageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Service.Services.NotificationServices.Discord
-{
-    public class DiscordMessageModel
-    {
-        public string username { get; set; }
-        public string avatar_url { get; set; }
-        public string content { get; set; }
-    }
-}
